e7f69f45db8c02a459dfe39d16165afa9dbcf2fc
Jens <jenswilliversen@gmail.com>
2021-01-05 15:35:58 +0100

    Updated Language.java

--- src/main/java/Spil/Language.java
+++ src/main/java/Spil/Language.java
@@ -6,12 +6,10 @@ import java.util.Properties;
 
 public class Language {
     private Properties prop = new Properties();
-    private FileInputStream stream;
-
 
     public Language(String fileName) {
         try {
-            stream = new FileInputStream("./Languages/" + fileName);
+            FileInputStream  stream = new FileInputStream("./Languages/" + fileName);
             prop.load(stream);
         } catch (FileNotFoundException e) {
 
