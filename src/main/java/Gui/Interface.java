2ad2fe2867f059f18d6cdc8c97fed0da668dc95c
baldm <63373006+baldm@users.noreply.github.com>
2021-01-05 15:00:29 +0100

    Lavet hurtig interface klasse

---
+++ src/main/java/Gui/Interface.java
@@ -0,0 +1,15 @@
+package Gui;
+
+import gui_fields.GUI_Player;
+import gui_main.GUI;
+
+public class Interface {
+    private static GUI gui;
+    private static GUI_Player[] guiPlayerList;
+
+    public Interface() {
+        gui = new GUI();
+    }
+
+
+}
---
+++ src/test/java/Gui/GuiBootTest.java
@@ -0,0 +1,7 @@
+package Gui;
+
+public class GuiBootTest {
+    public static void main(String[] args) {
+        Interface inter = new Interface();
+    }
+}
