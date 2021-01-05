66ce7c5cb1af866edca518ce3f7f980774950471
baldm <63373006+baldm@users.noreply.github.com>
2021-01-05 15:52:52 +0100

    Added methods to interface
    You can now update dices, set the player balance, display multiple buttons and display chance cards

--- src/main/java/Gui/Interface.java
+++ src/main/java/Gui/Interface.java
@@ -1,9 +1,13 @@
 package Gui;
 
+import Spil.Player;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_fields.GUI_Start;
 import gui_main.GUI;
+
+// Nedenstående kode er en modificeret udgave, med udgangspunkt i:
+// CDIO2 og CDIO3 af os, gruppe 15.
 
 public class Interface {
     private GUI gui;
@@ -30,8 +34,57 @@ public class Interface {
      * @param player - Player object
      */
     public void movePlayer(Player player){
-        gui.getFields()[player.getPosition()].setCar(player.id, true);
+        gui.getFields()[player.getPosition()].setCar(guiPlayerList[player.getID()], true);
     }
+
+    /**
+     * Display a message on the gui.
+     * @param message
+     */
+    public void displayMessage(String message) {
+        gui.showMessage("\n\n\n"+message);
+    }
+
+    /**
+     * Sets the dice on the gui
+     * @param roll1 Int
+     * @param roll2 Int
+     */
+    public void setBoardDice(int roll1, int roll2 ) {
+        gui.setDice(roll1, roll2);
+    }
+
+    /**
+     * tilføjer en delta balance til player
+     * @param player playerObject
+     */
+
+    public void setPlayerBalance(Player player) {
+        guiPlayerList[player.getID()].setBalance(player.getMoney());
+    }
+
+    /**
+     * Displays a message on the gui with  variable amount of choices
+     * @param msg String - A message before the choice
+     * @param args Strings seperated by comma
+     */
+    public String displayMultiButtonMsg(String msg, String... args) {
+        return gui.getUserSelection(msg, args);
+    }
+
+    /**
+     * Displays a chance card on the gui.
+     * @param stringInput
+     */
+    public void displayChance(String stringInput) {
+
+        // It seems to work best calling both of these commands at the same time.
+        gui.setChanceCard(stringInput);
+        gui.displayChanceCard(stringInput);
+    }
+
+
+
 
 
 }
