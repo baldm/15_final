156fea67ec3d62dbe77ad03d28a288060b506eec
baldm <63373006+baldm@users.noreply.github.com>
2021-01-05 15:13:01 +0100

    Tilføjet 2 metoder til interface
    findplayer og moveplayer metode

--- src/main/java/Gui/Interface.java
+++ src/main/java/Gui/Interface.java
@@ -1,14 +1,36 @@
 package Gui;
 
+import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
+import gui_fields.GUI_Start;
 import gui_main.GUI;
 
 public class Interface {
-    private static GUI gui;
-    private static GUI_Player[] guiPlayerList;
+    private GUI gui;
+    private GUI_Player[] guiPlayerList;
 
     public Interface() {
-        gui = new GUI();
+        GUI_Field[] fields = new GUI_Field[]{
+                new GUI_Start(),
+        };
+        gui = new GUI(fields);
+    }
+
+    /**
+     * Internal method for finding a player in the array of gui player objects
+     * @param playerID - Integer
+     * @return Specific gui Player Object
+     */
+    private GUI_Player findGuiPlayer(int playerID){
+        return guiPlayerList[playerID];
+    }
+
+    /**
+     * Method that moves a player on the grid
+     * @param player - Player object
+     */
+    public void movePlayer(Player player){
+        gui.getFields()[player.getPosition()].setCar(player.id, true);
     }
 
 
