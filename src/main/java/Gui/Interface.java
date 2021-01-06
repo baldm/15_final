37ba4d541f16a18f5d634104e5894edc32dd8a6b
baldm <63373006+baldm@users.noreply.github.com>
2021-01-06 11:33:35 +0100

    Added functionality to select player names and select a type of player icon

--- src/main/java/Gui/Interface.java
+++ src/main/java/Gui/Interface.java
@@ -2,6 +2,7 @@ package Gui;
 
 import Spil.Language;
 import Spil.Player;
+import gui_fields.GUI_Car;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_fields.GUI_Start;
@@ -29,13 +30,69 @@ public class Interface {
         String choice = displayMultiButtonMsg("Choose a language:", languages);
         return choice;
     }
+
+    /**
+     * Method for choosing player names, also creates the gui Player objects
+     * @param lang Language object
+     * @return Array of Strings containing player names
+     */
     public String[] initPlayerNames(Language lang) {
+        // Asks for the amount of players in the game
         String stringNumPlayers = displayMultiButtonMsg(lang.getString("EnterPlayerNumber"), "2", "3", "4", "5", "6");
         int numPlayers = Integer.parseInt(stringNumPlayers);
-        String[] playerArray = new String[numPlayers];
 
+        // Creates arrays to store player names and GUI player objects
+        String[] playerArray = new String[numPlayers];
+        guiPlayerList = new GUI_Player[numPlayers];
+
+        // Loops over players
         for (int i = 0; i < numPlayers; i++) {
             playerArray[i] = displayEnterStringMsg(lang.getString("EnterPlayerName"));
+            String playerIcon = displayMultiButtonMsg(
+                    lang.getString("EnterPlayerIcon"),
+                    lang.getString("Car"),
+                    lang.getString("Racecar"),
+                    lang.getString("Tractor"),
+                    "UFO");
+
+            // Handles the type of car
+            GUI_Car.Type type = GUI_Car.Type.CAR;
+            switch (playerIcon) {
+                case "Car":
+                    type = GUI_Car.Type.CAR;
+                    break;
+                case "Tractor":
+                    type = GUI_Car.Type.TRACTOR;
+                    break;
+                case "Racecar":
+                    type = GUI_Car.Type.RACECAR;
+                    break;
+                case "UFO":
+                    type = GUI_Car.Type.UFO;
+                    break;
+            }
+
+            // Creates the car object
+            GUI_Car car = new GUI_Car();
+            switch (i) {
+                case 0:
+                    car = new GUI_Car(Color.BLUE, Color.BLACK, type, GUI_Car.Pattern.FILL);
+                    break;
+                case 1:
+                    car = new GUI_Car(Color.RED, Color.BLACK, type, GUI_Car.Pattern.FILL);
+                    break;
+                case 2:
+                    car = new GUI_Car(Color.YELLOW, Color.BLACK, type, GUI_Car.Pattern.FILL);
+                    break;
+                case 3:
+                    car = new GUI_Car(Color.GREEN, Color.BLACK, type, GUI_Car.Pattern.FILL);
+                    break;
+                case 4:
+                    car = new GUI_Car(Color.PINK, Color.BLACK, type, GUI_Car.Pattern.CHECKERED);
+                    break;
+            }
+            // Adds player to storage
+            guiPlayerList[i] = new GUI_Player(playerArray[i], 30000, car);
         }
         return playerArray;
 
