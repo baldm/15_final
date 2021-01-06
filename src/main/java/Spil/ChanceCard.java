8a268e0be1dd0b8dbdd76ef5ea291e1eb30d5b47
Jens <jenswilliversen@gmail.com>
2021-01-06 21:49:57 +0100

    Added basic ChanceCard.java

--- src/main/java/Spil/ChanceCard.java
+++ src/main/java/Spil/ChanceCard.java
@@ -1,4 +1,31 @@
 package Spil;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ChanceCard {
+    private String cardName;
+
+
+    public  ChanceCard(String fileName, Language language) {
+        try {
+            FileInputStream input = new FileInputStream("./ChanceCards/"+ fileName);
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            cardName = prop.getProperty("name");
+            cardName = language.getString(cardName);
+
+
+
+        } catch (FileNotFoundException e){
+
+        } catch (IOException e){
+
+        }
+
+    }
 }
