095f206bfb32f3ab7fb1cfa954460b191ca41521
baldm <63373006+baldm@users.noreply.github.com>
2021-01-06 14:31:41 +0100

    Fixet bug mellem interface og game controller

--- src/main/java/Launcher/gameController.java
+++ src/main/java/Launcher/gameController.java
@@ -6,13 +6,15 @@ import Spil.LanguageScanner;
 import Spil.Player;
 
 public class gameController {
-    Player[] playerArray = new Player[];
+    private Player[] playerArray;
+    private Language lang;
 
     public static void main(String[] args) {
-        gameInit();
+        gameController game = new gameController();
+        game.gameInit();
     }
 
-    private static void gameInit() {
+    private void gameInit() {
 
         LanguageScanner langScanner = new LanguageScanner();
         String[] languageArray = langScanner.getLanguageNames();
@@ -20,20 +22,21 @@ public class gameController {
         // Creates a dialog for selecting a language
         Interface gameInterface = new Interface();
         String langChoice = gameInterface.initLanguage(languageArray);
-        Language lang = new Language(langChoice+".properties"); // Sets the language
+        lang = new Language(langChoice+".properties"); // Sets the language
 
         // Dialog for entering player names
         String[] playerStringArray = gameInterface.initPlayerNames(lang);
 
         // Creates player objects
+        playerArray = new Player[playerStringArray.length];
         for (int i = 0; i < playerStringArray.length; i++) {
-
+            playerArray[i] = new Player(playerStringArray[i], 30000, i);
         }
 
+        // Creates final game interface
         gameInterface.gameInit();
-
-
-
+    }
+    private void takeTurn() {
 
     }
 }
