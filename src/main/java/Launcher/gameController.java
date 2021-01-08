92a578b1e379a1f52f4d26063469d3c60ad70653
baldm <63373006+baldm@users.noreply.github.com>
2021-01-08 11:25:08 +0100

    added a simple take turn method

--- src/main/java/Launcher/gameController.java
+++ src/main/java/Launcher/gameController.java
@@ -9,12 +9,14 @@ import Spil.Player;
 public class gameController {
     private Player[] playerArray;
     private Language lang;
+    private Interface gameInterface;
     private Dice diceOne = new Dice(6);
     private Dice diceTwo = new Dice(6);
 
     public static void main(String[] args) {
         gameController game = new gameController();
         game.gameInit();
+        game.takeTurn();
     }
 
     private void gameInit() {
@@ -23,7 +25,7 @@ public class gameController {
         String[] languageArray = langScanner.getLanguageNames();
 
         // Creates a dialog for selecting a language
-        Interface gameInterface = new Interface();
+        gameInterface = new Interface();
         String langChoice = gameInterface.initLanguage(languageArray);
         lang = new Language(langChoice+".properties"); // Sets the language
 
@@ -41,6 +43,10 @@ public class gameController {
     }
     private void takeTurn() {
         // Simple turn
-        diceOne.roll();
+        gameInterface.displayMessage(lang.getString("RollDice"));
+        int sumRolls = diceOne.Roll() + diceTwo.Roll();
+        gameInterface.setBoardDice(diceOne.getValue(), diceTwo.getValue());
+        gameInterface.displayMessage(lang.getString("DiceResult")+" "+diceOne.getValue()+" & "+diceTwo.getValue());
+
     }
 }
