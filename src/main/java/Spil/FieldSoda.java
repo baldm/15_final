98bdc19858f08d36704d118d4d1bf88523a957a2
Jens <jenswilliversen@gmail.com>
2021-01-11 12:47:38 +0100

    Added Cola and Squash field

--- src/main/java/Spil/FieldSoda.java
+++ src/main/java/Spil/FieldSoda.java
@@ -1,5 +1,47 @@
 package Spil;
 
-public class FieldSoda extends Field {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class FieldSoda extends Field {
+    private int price;
+    private int[] rentMultiplier;
+    private int groupID;
+    private int mortageValue;
+
+    public FieldSoda(String fileName){
+        try {
+            FileInputStream input = new FileInputStream("./Field/FieldSoda/"+ fileName);
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+
+            name = prop.getProperty("name");
+            position = Integer.parseInt(prop.getProperty("pos"));
+            fieldType = Integer.parseInt(prop.getProperty("fieldType"));
+            fieldID = Integer.parseInt(prop.getProperty("fieldID"));
+
+            price = Integer.parseInt(prop.getProperty("price"));
+            String[] rents = prop.getProperty("rent").split(",");
+
+            rentMultiplier = new int[rents.length];
+            for(int i=0;i<rents.length;i++){
+                rentMultiplier[i] = Integer.parseInt(rents[i]);
+            }
+
+            mortageValue = price;
+
+
+
+
+
+        } catch (FileNotFoundException e){
+
+        } catch (IOException e){
+
+        }
+    }
 }
