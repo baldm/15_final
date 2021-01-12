b2329d4f4efcaa83331a2c6b6b4cd6462176b6c6
Jens <jenswilliversen@gmail.com>
2021-01-12 20:01:21 +0100

    added drawChancecard

--- src/main/java/Launcher/gameController.java
+++ src/main/java/Launcher/gameController.java
@@ -109,10 +109,10 @@ public class gameController {
 
     private void drawChanceCard(Player player) {
         gameInterface.displayMessage(lang.getString("LandedOnChancecard"));
-
+        ChanceCard drawedCard;
         if(drawAbleChanceCards.length > 1){
             int drawedCardNumber = ThreadLocalRandom.current().nextInt(0, drawAbleChanceCards.length);
-            ChanceCard drawedCard = drawAbleChanceCards[drawedCardNumber];
+            drawedCard = drawAbleChanceCards[drawedCardNumber];
             ChanceCard[] chanceCardsPlaceholder = drawAbleChanceCards.clone();
             drawAbleChanceCards = new ChanceCard[drawAbleChanceCards.length-1];
 
@@ -122,9 +122,18 @@ public class gameController {
                 }
             }
         } else {
-            ChanceCard drawedCard = drawAbleChanceCards[0];
+            drawedCard = drawAbleChanceCards[0];
             drawAbleChanceCards = new ChanceCard[allChanceCards.length];
             drawAbleChanceCards = allChanceCards.clone();
+        }
+
+        switch (drawedCard.cardGroup){
+            case 2:
+                player.addMoney(((ChanceCardChangeMoney) drawedCard).getMoneyChange());
+            case 3:
+                player.setPosition(((ChanceCardMovePlayerTo) drawedCard).getMoveTo());
+            case 4:
+                player.setPosition(player.getPosition() + ((ChanceCardMovePlayer) drawedCard).getMovePlayer());
         }
 
     }
