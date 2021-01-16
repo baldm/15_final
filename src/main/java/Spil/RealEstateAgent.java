0adb4e55d203c19cb6317f6101f52782162bb3f9
Jens <jenswilliversen@gmail.com>
2021-01-16 13:26:38 +0100

    RealEstateAgent update

--- src/main/java/Spil/RealEstateAgent.java
+++ src/main/java/Spil/RealEstateAgent.java
@@ -1,14 +1,17 @@
 package Spil;
+import Spil.ChanceCards.ChanceCardChangeMoney;
 import Spil.Fields.*;
 
 public class RealEstateAgent {
      Field[] fieldArray;
      Player[] owners;
      int[] fieldType1 = new int[0];
+     int[][] fieldType1Sorted;
      int[] fieldType2 = new int[0];
 
     public RealEstateAgent(Field[] fieldInput) {
         // Creates fields
+        int totalGroupNumber = 0;
 
         fieldArray = fieldInput;
         owners = new Player[fieldArray.length];
@@ -42,7 +45,43 @@ public class RealEstateAgent {
                     break;
 
             }
+
+
         }
+
+        for (int i=0; i < fieldType1.length;i++) {
+            if(totalGroupNumber < ((FieldProperty) fieldArray[fieldType1[i]]).getGroupID()+1){
+                totalGroupNumber = ((FieldProperty) fieldArray[fieldType1[i]]).getGroupID()+1;
+            }
+
+        }
+
+        fieldType1Sorted = new int[totalGroupNumber][];
+
+
+
+        for (int i=0;i<fieldType1.length;i++) {
+            int[] placeholder;
+            int input =((FieldProperty) fieldArray[fieldType1[i]]).getGroupID();
+
+
+            placeholder = fieldType1Sorted[input].clone();
+            fieldType1Sorted[input] = new int[placeholder.length+1];
+
+            int n;
+            for (n=0; n< placeholder.length;n++){
+               fieldType1Sorted[input][n] = placeholder[n];
+            }
+            fieldType1Sorted[input][n] = fieldType1[i];
+
+
+        }
+
+
+
+
+
+
 
     }
 
@@ -59,34 +98,9 @@ public class RealEstateAgent {
        switch (field.fieldType){
            case 1:
 
-               for (int k=0;k<fieldType1.length;k +=3){
-                   if(k+3 < fieldType1.length){
-                       if(owners[field.position] != null && owners[fieldType1[k]].getID() == owners[fieldType1[k+1]].getID()){
-                           return true;
-                       }else{
-                           return false;
-                       }
-                   } else if(k==0){
-                       if(field.fieldID < k+2){
 
 
-                            if(owners[field.position] != null && owners[fieldType1[k]].getID() == owners[fieldType1[k+1]].getID()){
-                            return true;
-                            }else{
-                             return false;
-                            }
-                       }else{
-                           k--;
-                       }
 
-                   }else if(field.fieldID < k+3  ){
-                       if(owners[field.position] != null && owners[fieldType1[k]].getID() == owners[fieldType1[k+1]].getID() && owners[fieldType1[k]].getID() == owners[fieldType1[k+2]].getID()){
-                           return true;
-                       } else {
-                           return false;
-                       }
-                   }
-               }
            case 2:
                if(owners[field.position] != null && owners[fieldType2[0]].getID() == owners[fieldType2[1]].getID() && owners[fieldType2[0]].getID() == owners[fieldType2[2]].getID() && owners[fieldType2[0]].getID() == owners[fieldType2[3]].getID()){
                    return true;
@@ -95,7 +109,7 @@ public class RealEstateAgent {
                }
 
            default:
-               throw new RuntimeException("Error in field reader");
+               throw new RuntimeException("Error. Not a buyable field");
 
         }
     }
